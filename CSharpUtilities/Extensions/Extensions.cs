c9c1d088f9fb4a219215c5b76700a26554fc2d34
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2020-04-25 19:46:24 +0100

    added float equal util

---
+++ CSharpUtilities/Extensions/Extensions.cs
@@ -0,0 +1,29 @@
+﻿namespace MP.CSharpUtilities.Extensions
+{
+    public static class Extensions
+    {
+        public static bool AlmostEqual(this float val, float other, float tolerance = 0.005f)
+        {
+            return val >= other - tolerance && val <= other + tolerance;
+        }
+
+        public static bool AlmostZero(this float val, float tolerance = 0.005f)
+        {
+            return AlmostEqual(val, 0f, tolerance);
+        }
+
+        public static float Clamp(this float val, float min, float max)
+        {
+            if(val > max)
+            {
+                val = max;
+            }
+            if(val < min)
+            {
+                val = min;
+            }
+
+            return val;
+        }
+    }
+}
---
+++ CSharpUtilitiesTest/ExtensionsTest.cs
@@ -0,0 +1,17 @@
+﻿using MP.CSharpUtilities.Extensions;
+using NUnit.Framework;
+
+namespace CSharpUtilitiesTest
+{
+    [TestFixture]
+    public class ExtensionsTest
+    {
+        [Test]
+        public void AlmostEqualTest()
+        {
+            float num = 5f;
+            Assert.That(num.AlmostEqual(5.0001f), Is.True);
+            Assert.That(num.AlmostEqual(5.1f), Is.False);
+        }
+    }
+}
